a188eac48de70ccd8dbbd015ada2e4859648620f
hellofatimad <fatimatnhd2021@gmail.com>
2023-05-11 15:30:54 -0700

    labweek6 update script

--- grade.sh
+++ grade.sh
@@ -8,9 +8,42 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+# change the directory to student-submission
+cd student-submission
+
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+if [[ -f ListExamples.java ]]
+then
+    echo 'found file'
+else 
+    echo 'no file found'
+fi
+
+cd ../
+
+# move the student files, tests, and lib 
+cp student-submission/ListExamples.java grading-area
+cp TestListExamples.java grading-area
+cp -r lib grading-area
+
+ls grading-area
+
+# compile the files in grading-area
+cd grading-area
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar ListExamples.java TestListExamples.java
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+
+if [[ $? -eq 0 ]]
+then 
+    echo "files passed"
+
+else 
+    echo "failure in file"
+
+fi
